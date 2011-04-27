e867fb55df107d197d113c7f61a8a12848320e03
dirck <none@none>
2011-04-27 05:46:13 +0000

    neck charter updates
    --HG--
    branch : pytabla

--- pyneck.py
+++ pyneck.py
@@ -2,6 +2,14 @@
     pyneck.py  input.txt   output.pdf
 """
 
+"""
+    X Y movement in input is weird because X Y is happening on output
+
+    the Chart could draw during input instead...
+
+    let's go with:  either the charts are all placed or none are.
+"""
+
 import string
 import sys
 
@@ -22,38 +30,98 @@ class Bunch:
         self.__dict__.update(kwds)
 
 
+# change naming conventions for globals?
+
 pageSize=(8.5*inch,11*inch)
 margins = Bunch(left=1, right=1, top=1, bottom=1)
 
-startX, startY = margins.left * inch, pageSize[1] - margins.top * inch
+# init function for cases where any of these globals are changed
+
+startX = margins.left * inch
+startY = pageSize[1] - margins.top * inch
+X = startX
+Y = startY
 nRows = 5
 nCols = 5
+
+chartWidth = (pageSize[0] - (margins.left+margins.right)*inch) / nCols
+chartHeight = (pageSize[1] - (margins.top+margins.bottom)*inch) / nRows
+
 lineWidth = inch/144    # fine lines
-cellWidth = inch / 5.
-cellHeight = inch / 4.
+cellWidth = chartWidth / 8.
+cellHeight = cellWidth * 1.4
+
 dotRadius = cellWidth / 3.
 fontName="Times-Roman"
 fontSize=9
 
+Charts = []
+
 def main(argv):
+    global X,Y
     if len(argv) != 2:
         print __doc__
         sys.exit(2);
     c = Canvas(argv[1], pagesize=pageSize, pageCompression=0)
     c.setFont(fontName, fontSize)
-    chart = Chart(title="D Major");
-    chart.marks += [ (6,0,'x'), (5,0,'x'), (4,0,'o'), (3,2,'1'), (2,3,'3'), (1, 2, '2')]
-    chart.draw(c, startX, startY)
+
+    X = 0
+    Y = 0
+    execfile(argv[0])
+    X = startX
+    Y = startY
+    for chart in Charts:
+        x = chart.fx
+        y = chart.fy
+        if not x and not y:
+            x = X
+            y = Y
+        chart.draw(c, x, y)
+        over()
+
+    #chart = Chart(title="D Major");
+    #chart.marks += [ (6,0,'x'), (5,0,'x'), (4,0,'o'), (3,2,'1'), (2,3,'3'), (1, 2, '2')]
+    #chart.draw(c, startX, startY)
     c.showPage()
     c.save()
 
+# move the "cursor"
+# back, down, right, bottom
+
+def over():
+    global X
+    X = X + chartWidth
+    if (pageSize[0] - X) < chartWidth:
+        next()
+
+def down():
+    global X,Y
+    #X = startX
+    Y = Y - chartHeight
+
+def next():
+    down()
+    left()
+
+def left():
+    global X
+    X = startX
+
+def top():
+    global Y
+    Y = startY
+
 class Chart:
-    def __init__(self, frets=4, title=""):
+    def __init__(self, title="", frets=4):
+        global X, Y
+        Charts.append(self)
         self.frets = frets
         self.title = title
         self.dots = []
         self.circles = []
         self.marks = []
+        self.fx = X
+        self.fy = Y
 
     def frame(self):
         c = self.canvas
@@ -71,8 +139,31 @@ class Chart:
         self.circles.append( (string, fret) )
     def mark(self, string, fret, s):
         self.marks.append( (string, fret, s) )
-        if fret != 0:
-            self.circles.append( (string, fret) )
+
+    def draw_dot(self, string, fret):
+        c = self.canvas
+        x = self.fx + (6-string) * cellWidth
+        y = self.fy - (fret) * cellHeight + cellHeight/2
+        c.circle(x, y, dotRadius, 0, 1)
+
+    def draw_circle(self, string, fret):
+        c = self.canvas
+        x = self.fx + (6-string) * cellWidth
+        y = self.fy - (fret) * cellHeight + cellHeight/2
+        c.circle(x, y, dotRadius, 1, 1)
+
+    def draw_mark(self, string, fret, mark):
+        c = self.canvas
+        x = self.fx + (6-string) * cellWidth
+        if fret == 0:
+            y = self.fy + fontSize * .2
+        else:
+            y = self.fy - (fret) * cellHeight + cellHeight / 2 - fontSize / 3
+            c.setFillColorRGB(1,1,1)
+            if string > 0 and string < 7 and mark[0] != '(':    #OPT
+                self.draw_circle(string, fret)
+            c.setFillColorRGB(0,0,0)
+        c.drawCentredString(x, y, mark)
 
     def draw(self, c, fx, fy):
         self.canvas = c
@@ -83,20 +174,13 @@ class Chart:
             c.drawCentredString(fx+cellWidth*2.5, fy+fontSize, self.title)
         c.setFillColorRGB(0,0,0)
         for string,fret in self.dots:
-            x = fx + (6-string) * cellWidth
-            y = fy - (fret) * cellHeight + cellHeight/2
-            c.circle(x, y, dotRadius, 0, 1)
+            self.draw_dot(string, fret)
         c.setFillColorRGB(1,1,1)
         for string,fret in self.circles:
-            x = fx + (6-string) * cellWidth
-            y = fy - (fret) * cellHeight + cellHeight/2
-            c.circle(x, y, dotRadius, 1, 1)
+            self.draw_circle(string, fret)
         c.setFillColorRGB(0,0,0)
-        for string, fret, s in self.marks:
-            x = fx + (6-string) * cellWidth
-            y = fy - (fret) * cellHeight + fontSize * .65
-            c.drawCentredString(x, y, s)
-
+        for string, fret, mark in self.marks:
+            self.draw_mark(string, fret, mark)
 
 if __name__ == '__main__':
     main(sys.argv[1:])
